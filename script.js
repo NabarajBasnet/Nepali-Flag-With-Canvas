0a76b8c1d66441f50a96ac8569cf50c93d577daa
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-12-30 16:59:45 +0545

    Creating star

--- script.js
+++ script.js
@@ -1,38 +1,78 @@
 const flag = document.getElementById('flag');
 const ctx = flag.getContext('2d');
 
-ctx.strokeStyle = 'blue';
-ctx.fillStyle = 'red';
-ctx.lineWidth = 30;
-// ctx.beginPath();
+const createFlag = () => {
+    ctx.strokeStyle = 'blue';
+    ctx.fillStyle = 'red';
+    ctx.lineWidth = 30;
+    ctx.beginPath();
 
-ctx.moveTo(120, 65);
+    ctx.moveTo(120, 65);
 
-ctx.lineTo(120, 500);
-ctx.lineTo(120, 500);
-ctx.lineTo(500, 500);
-ctx.lineTo(240, 280);
-ctx.lineTo(500, 280);
-ctx.lineTo(120, 70);
-ctx.lineTo(120, 500);    // Handle
+    ctx.lineTo(120, 500);
+    ctx.lineTo(120, 500);
+    ctx.lineTo(500, 500);
+    ctx.lineTo(240, 280);
+    ctx.lineTo(500, 280);
+    ctx.lineTo(120, 70);
+    ctx.lineTo(120, 500);
 
-ctx.stroke();
-ctx.fill();
+    ctx.stroke();
+    ctx.fill();
+}
+createFlag();
 
 // Moon
-ctx.beginPath();
-ctx.fillStyle = 'white';
-ctx.lineWidth = 20
-ctx.strokeStyle = 'white';
-ctx.arc(210, 200, 20, 0, 1 * Math.PI);
-ctx.stroke();
+const drawMoon = () => {
+
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'white';
+    ctx.arc(210, 190, 40, 0, Math.PI);
+    ctx.stroke();
+    ctx.fill();
+
+
+}
+drawMoon();
 
 // Draw Star
-const drawStar = ()=>{
-ctx.beginPath();
-ctx.lineWidth = 2
-ctx.moveTo(220,300);
-ctx.lineTo(200,320)
-ctx.stroke();
-}
-drawStar();
+const drawStar = (cx, cy, spikes, Or, Ir) => {
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'white';
+
+    let x = cx;
+    let y = cy;
+    const w = cx + 40;
+    const h = cy + 40;
+
+    ctx.moveTo(x - 20, y - 50)
+    ctx.lineTo(x - 10, y - 60)
+    ctx.lineTo(x, y - 50)
+    ctx.lineTo(x - 20, cy - 50)
+    ctx.fill()
+    ctx.stroke();
+
+    for (let i = 1; i <= spikes; i++) {
+
+        x = x - 17
+        y = y - 5
+        ctx.moveTo(x - 20, y - 50)
+        ctx.lineTo(x - 10, y - 60)
+        ctx.lineTo(x, y - 50)
+        ctx.lineTo(x - 20, cy - 50)
+        ctx.arc(x, y, 40, 0, Math.PI);
+        ctx.fillStyle = 'white'
+        ctx.fill()
+        ctx.stroke();
+
+        // ctx.moveTo(200, 400);
+        // ctx.lineTo(cx, cy);
+
+    }
+    ctx.stroke();
+
+};
+drawStar(220, 410, 12);
